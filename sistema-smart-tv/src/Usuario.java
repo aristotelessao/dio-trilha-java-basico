fd49ec645c174dfd23e64574bed2fa58300e51cc
aristoteleso <aristoteleso@hotmail.com>
2023-10-26 20:44:35 -0300

    Arquivos durante treinamento

---
+++ sistema-smart-tv/src/Usuario.java
@@ -0,0 +1,27 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV ligada? " + smartTv.ligada);
+        smartTv.ligar();
+        System.out.println("TV ligada? " + smartTv.ligada);
+        smartTv.desligar();
+        System.out.println("TV ligada? " + smartTv.ligada);
+
+        System.out.println("Volume atual: " + smartTv.volume);
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.diminuirVolume();
+        smartTv.aumentarVolume();
+        System.out.println("Volume atual: " + smartTv.volume);
+
+        System.out.println("");
+
+        System.out.println("Canal atual : " + smartTv.canal);
+        smartTv.aumentarCanal();
+        smartTv.aumentarCanal();
+        smartTv.diminuirCanal();
+        System.out.println("Canal atual : " + smartTv.canal);
+    }
+}
